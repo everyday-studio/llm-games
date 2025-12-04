69efd0af347e7284c73c2111985c31a0f926cc6b
Hojoong Jang <hojoong310@gmail.com>
2025-12-04 23:43:30 +0900

    Feat/di with fx (#5)
    * feat: get fx
    
    * feat: add user routes
    
    * fix: router invoke
    
    * style: db

--- apps/backend/cmd/server/main.go
+++ apps/backend/cmd/server/main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"flag"
 	"fmt"
 	"log"
 
 	"github.com/labstack/echo/v4"
+	"go.uber.org/fx"
 
 	"github.com/mondayy1/llm-games/internal/config"
 	"github.com/mondayy1/llm-games/internal/db"
@@ -15,13 +18,34 @@ import (
 )
 
 func main() {
-	// YAML Load
+	app := fx.New(
+		fx.Provide(
+			NewConfig,
+			NewDB,
+			echo.New,
+		),
+		fx.Provide(
+			usecase.NewUserUseCase,
+		),
+		fx.Provide(
+			repository.NewUserRepository,
+		),
+		fx.Invoke(
+			handler.NewUserHandler,
+		),
+		fx.Invoke(StartServer),
+	)
+
+	app.Run()
+}
+
+func NewConfig() *config.Config {
 	env := flag.String("env", "dev", "Environment (dev, prod)")
 	flag.Parse()
 
 	validEnvs := map[string]bool{"dev": true, "prod": true}
 	if !validEnvs[*env] {
-		log.Fatalf("Invalid environment: %s", *env)
+		log.Fatalf("Invalid environment: %s. Valid environments are: dev, prod", *env)
 	}
 
 	cfg, err := config.LoadConfig(*env)
@@ -30,24 +54,36 @@ func main() {
 	}
 	fmt.Printf("config: %+v\n", cfg)
 
+	return cfg
+}
+
+func NewDB(lc fx.Lifecycle, cfg *config.Config) *sql.DB {
 	dbConn, err := db.NewDBConnection(cfg)
 	if err != nil {
 		log.Fatalf("DB connection error: %v", err)
 	}
 
-	userRepo := repository.NewUserRepository(dbConn)
-	userUseCase := usecase.NewUserUseCase(userRepo)
-	userHandler := handler.NewUserHandler(userUseCase)
+	lc.Append(fx.Hook{
+		OnStop: func(ctx context.Context) error {
+			return dbConn.Close()
+		},
+	})
 
-	//Server
-	e := echo.New()
+	return dbConn
+}
 
-	e.POST("/users", userHandler.CreateUser)
-	e.GET("/users/:id", userHandler.GetByID)
-	e.GET("/users", userHandler.GetAll)
-
-	log.Printf("Server started at %s", fmt.Sprintf(":%d", cfg.App.Port))
-	if err := e.Start(fmt.Sprintf(":%d", cfg.App.Port)); err != nil {
-		log.Fatalf("failed to start server: %v", err)
-	}
+func StartServer(lc fx.Lifecycle, e *echo.Echo, cfg *config.Config) {
+	lc.Append(fx.Hook{
+		OnStart: func(ctx context.Context) error {
+			go func() {
+				if err := e.Start(fmt.Sprintf(":%d", cfg.App.Port)); err != nil {
+					log.Fatal("Shutting down the server due to:", err)
+				}
+			}()
+			return nil
+		},
+		OnStop: func(ctx context.Context) error {
+			return e.Shutdown(ctx)
+		},
+	})
 }
