bbac740eaac75b5d4c8fa7ea9b7f9cf01f434495
mondayy1 <hojoong310@gmail.com>
2025-12-15 16:35:23 +0900

    feat: auth repo createuser method

--- apps/backend/internal/repository/postgres/auth_repository.go
+++ apps/backend/internal/repository/postgres/auth_repository.go
@@ -3,6 +3,9 @@ package postgres
 import (
 	"context"
 	"database/sql"
+	"fmt"
+
+	"github.com/lib/pq"
 
 	"github.com/everyday-studio/ollm/internal/domain"
 )
@@ -18,6 +21,23 @@ func NewAuthRepository(db *sql.DB) domain.AuthRepository {
 }
 
 func (r *authRepository) CreateUser(ctx context.Context, user *domain.User) (*domain.User, error) {
-	//TODO
+	//Add User Role if no default role
+	if user.Role == "" {
+		user.Role = domain.RoleUser
+	}
+
+	const query = `
+		INSERT INTO users (name, email, password, role)
+		VALUES ($1, $2, $3, $4)
+		RETURNING id
+	`
+
+	if err := r.db.QueryRowContext(ctx, query, user.Name, user.Email, user.Password, user.Role).Scan(&user.ID); err != nil {
+		if pqErr, ok := err.(*pq.Error); ok && pqErr.Code == "23505" {
+			return nil, fmt.Errorf("email %s: %w", user.Email, domain.ErrAlreadyExists)
+		}
+		return nil, fmt.Errorf("failed to save user: %w", err)
+	}
+
 	return user, nil
 }
