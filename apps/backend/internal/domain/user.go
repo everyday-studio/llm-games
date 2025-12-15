e1f133647be89063ec2699d2729b3a2eef9a469d
mondayy1 <hojoong310@gmail.com>
2025-12-15 15:54:06 +0900

    feat: add role to user domain

--- apps/backend/internal/domain/user.go
+++ apps/backend/internal/domain/user.go
@@ -2,29 +2,31 @@ package domain
 
 import "context"
 
+type Role string
+
 const (
-	RolePublic  = "Public" // Token Unnecessary
-	RoleAdmin   = "Admin"
-	RoleManager = "Manager"
-	RoleUser    = "User"
+	RolePublic  Role = "Public" // public access, token unnecessary
+	RoleAdmin   Role = "Admin"
+	RoleManager Role = "Manager"
+	RoleUser    Role = "User"
 )
 
 type User struct {
-	ID       int64    `json:"id"`
-	Name     string   `json:"name"`
-	Email    string   `json:"email"`
-	Password string   `json:"-"`
-	Roles    []string `json:"roles"`
+	ID       int64  `json:"id"`
+	Name     string `json:"name"`
+	Email    string `json:"email"`
+	Password string `json:"-"`
+	Role     Role   `json:"role"`
 }
 
 type UserRepository interface {
-	Save(user *User) (*User, error)
-	GetByID(id int64) (*User, error)
-	GetAll() ([]User, error)
+	Save(ctx context.Context, user *User) (*User, error)
+	GetByID(ctx context.Context, id int64) (*User, error)
+	GetAll(ctx context.Context) ([]User, error)
 	GetUserByEmail(ctx context.Context, email string) (*User, error)
 }
 
 type UserUseCase interface {
-	GetByID(id int64) (*User, error)
-	GetAll() ([]User, error)
+	GetByID(ctx context.Context, id int64) (*User, error)
+	GetAll(ctx context.Context) ([]User, error)
 }
